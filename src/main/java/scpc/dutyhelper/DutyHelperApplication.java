f057fbebd4546e0f21406d87ce0c4d3b8c7c9d28
Mykhailo <101kutuzov@gmail.com>
2023-02-19 10:43:43 +0200

    Initial commit

---
+++ src/main/java/scpc/dutyhelper/DutyHelperApplication.java
@@ -0,0 +1,15 @@
+package scpc.dutyhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class DutyHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DutyHelperApplication.class, args);
+    }
+}
+
